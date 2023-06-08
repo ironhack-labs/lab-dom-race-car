7db663bc28e33ce9f90dcc9a2e8fbbe2c28ed82a
Devin Fiachra <devinduffymeade@gmail.com>
2023-06-08 18:57:49 +0200

    game solved with codealong

--- js/script.js
+++ js/script.js
@@ -2,11 +2,56 @@ window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
 
+  const game = new Game();
+
+  function startGame() {
+    game.start();
+  }
+
+  function restartGame() {
+    location.reload();
+  }
   startButton.addEventListener("click", function () {
     startGame();
   });
 
-  function startGame() {
-    console.log("start game");
+  restartButton.addEventListener("click", function () {
+    // Call the restartGame function when the button is clicked
+    restartGame();
+  });
+
+  function handleKeydown(event) {
+    const key = event.key;
+
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the pressed key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -5;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -10;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 5;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 5;
+          break;
+      }
+
+      game.player.move();
+    }
   }
+
+  window.addEventListener("keydown", handleKeydown);
 };

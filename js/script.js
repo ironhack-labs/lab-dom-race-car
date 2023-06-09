eaa3243010da7553438e49405cb6daa811c21af3
Mirza Abdullah Baig <abdullah.baig2013@gmail.com>
2023-06-09 09:04:11 +0200

    Solved lab 9

--- js/script.js
+++ js/script.js
@@ -1,12 +1,48 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  let game;
   startButton.addEventListener("click", function () {
     startGame();
   });
 
   function startGame() {
-    console.log("start game");
+    console.log("start gameeeeeeee");
+    game = new Game();
+    game.start();
   }
+  ///////////////////////////event
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the pressed key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      console.log(key);
+      event.preventDefault();
+      // Update player's directionX and directionY based on the key pressed
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
+
+  // Add the handleKeydown function as an event listener for the keydown event
+  window.addEventListener("keydown", handleKeydown);
 };

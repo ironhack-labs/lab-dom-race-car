40d3bcedf1fcd669e15c2a0bc76ba8d6fe472cce
Hassan Abou Saada <abousaada.hassan@gmail.com>
2023-06-09 17:47:40 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  // comments to be removed
   startButton.addEventListener("click", function () {
     startGame();
   });

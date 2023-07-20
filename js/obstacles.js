61bd66c614b1e3a48b94edc75a473f7292eb566d
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-07-20 18:17:40 +0100

    done

---
+++ js/obstacles.js
@@ -0,0 +1,35 @@
+class Obstacle {
+    constructor(greenScreen){
+        this.greenScreen = greenScreen;
+
+    	// random position for the appearance of the obstacles
+    	this.left = Math.floor(Math.random() * 300 + 70);
+
+    	// appear on top
+    	this.top = 0;
+    	this.width = 100;
+    	this.height = 150;
+
+    	// create the HTML element and default styling
+    	this.element = document.getElementById("img");
+    	this.element.src = "./images/redCar.png";
+    	this.element.style.position = "absolute";
+        this.element.style.top = `${this.top}px`;
+        this.element.style.left = `${this.left}px`;
+        this.element.style.height = `${this.height}px`;
+    	this.element.style.width = `${this.width}px`;
+
+        this.gameScreen.appendChild(this.element);
+    }
+
+    updatePosition(){
+    	this.element.style.left = `${this.left}px`;
+    	this.element.style.top = `${this.top}px`;
+    }
+
+    move(){
+        /// Drop the obstacle 3px to the bottom
+        this.top += 3;
+        this.updatePosition();
+    }
+}

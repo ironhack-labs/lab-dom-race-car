93150646f5bd867c58cf5fee1d456cc7160fc4b8
Mirabela Tiugan <mirabelatiugan@gmail.com>
2023-08-17 17:17:18 +0200

    Lab done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,82 @@
 class Game {
-    // code to be added
+  // code to be added
+  constructor() {
+    this.startScreen = document.querySelector("#game-intro");
+    this.gameScreen = document.querySelector("#game-screen");
+    this.gameEndScreen = document.querySelector("#game-end");
+    this.player = new Player(
+      this.gameScreen,
+      200,
+      500,
+      100,
+      150,
+      "./images/car.png"
+    );
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+  }
+
+  start() {
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameLoop();
+  }
+
+  gameLoop() {
+    if (this.gameIsOver) {
+      return;
+    }
+    this.update();
+    window.requestAnimationFrame(() => this.gameLoop());
+  }
+
+  update() {
+    console.log("update");
+    this.player.move();
+
+    // Create a new obstacle based on a random probability
+    // when there is no other obstacles on the screen
+    if (Math.random() > 0.98 && this.obstacles.length < 1) {
+      this.obstacles.push(new Obstacle(this.gameScreen));
+    }
+
+    // make obstacle move
+    if (this.obstacles.length) {
+      const obstacle = this.obstacles[0];
+      obstacle.move();
+
+      // avoid colision
+      if (obstacle.top > this.height) {
+        this.score++;
+
+        obstacle.element.remove();
+        this.obstacle.splice(0, 1);
+      }
+
+      if (this.obstacles.length && this.player.didColide(obstacle)) {
+        this.lives--;
+        console.log("lives:", this.lives);
+        obstacle.element.remove();
+        this.obstacles.splice(0, 1);
+      }
+    }
+
+    if (this.lives <= 0) {
+      this.endGame();
+    }
+  }
+
+  endGame() {
+    this.player.element.remove();
+    this.obstacles.forEach((obstacle) => obstacle.element.remove());
+    this.gameIsOver = true;
+    this.gameScreen.style.display = "none";
+    this.gameEndScreen.style.display = "block";
+  }
 }
--- js/script.js
+++ js/script.js
@@ -2,11 +2,48 @@ window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
 
+  let game = new Game();
+
   startButton.addEventListener("click", function () {
     startGame();
   });
 
   function startGame() {
     console.log("start game");
+   // game = new Game(); // added
+    game.start(); // added
+  }
+
+  //## Iteration 5: Handle Keyboard Input
+  window.addEventListener("keydown", (event) => {
+    // console.log(event.key);
+    event.preventDefault();
+
+    switch(event.key){
+      case "ArrowLeft":
+        game.player.directionX = -1;
+        break;
+      case "ArrowUp":
+        game.player.directionY = -1;
+        break;
+      case "ArrowRight":
+        game.player.directionX = 1;
+        break;
+      case "Arrowleft":
+        game.player.directionY = 1;
+      break;
+    }
+  });
+
+  // ## Iteration 8: End Game Screen
+
+    // Add an event listener to the restart button & Call the restartGame function when the button is clicked
+    restartButton.addEventListener("click", function () {
+      restartGame();
+    });
+
+      // The function that reloads the page to start a new game
+    function restartGame() {
+    location.reload();
   }
 };

c1408e199dc08a996b8b5c92ba8f98bdfdce6907
João Cravidão <joao_kravidao@hotmail.com>
2023-10-19 21:14:00 +0100

    done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,99 @@
 class Game {
-    // code to be added
-}
+    constructor() {
+      this.startScreen = document.getElementById("game-intro");
+      this.gameScreen = document.getElementById("game-screen");
+      this.gameEndScreen = document.getElementById("game-end");
+      this.player = new Player(this.gameScreen, 200, 500, 100, 150, "./images/greenCar.png");
+      this.height = 600;
+      this.width = 500;
+      this.obstacles = [];
+      this.score = 0;
+      this.lives = 3;
+      this.gameIsOver = false;
+      this.loadingObstacle = false;
+    }
+  
+    start() {
+      // Set the height and width of the game screen
+      this.gameScreen.style.height = `${this.height}px`;
+      this.gameScreen.style.width = `${this.width}px`;
+  
+      // Hide the start screen
+      this.startScreen.style.display = "none";
+      
+      // Show the game screen
+      this.gameScreen.style.display = "block";
+  
+      // Start the game loop
+      this.gameLoop();
+    }
+  
+    gameLoop() {
+      console.log("in the game loop");
+  
+      // Interrupt the function to stop the loop if "gameIsOver" is set to "true"
+      if (this.gameIsOver) {
+        return;
+      }
+  
+      this.update();
+  
+      window.requestAnimationFrame(() => this.gameLoop());
+    }
+  
+    update() {
+      console.log("in the update");
+      let score = document.getElementById("score");
+      let lives = document.getElementById("lives");
+
+      score.innerHTML = this.score;
+      lives.innerHTML = this.lives;
+
+      this.player.move()
+
+      for (let i = 0; i<this.obstacles.length; i++) {
+        const obstacle = this.obstacles[i];
+        obstacle.move();
+
+        // Check for collision
+        if (this.player.didCollide(obstacle)){
+          obstacle.element.remove();
+
+          this.obstacles.splice(i,1);
+
+          this.lives --;
+        }
+
+        else if(obstacle.top > this.height){
+          this.score++;
+          obstacle.element.remove();
+          this.obstacles.splice(i,1);
+        }
+      }
+
+      if (this.lives === 0){
+        this.endGame();
+      }
+
+      if(!this.obstacles.length && !this.loadingObstacle && !this.gameIsOver) {
+        this.loadingObstacle = true;
+        setTimeout(()=>{
+          this.obstacles.push(new Obstacle(this.gameScreen));
+          this.loadingObstacle = false;
+        }, 500);
+      }
+
+    }
+
+    endGame(){
+      this.gameIsOver = true;
+      this.player.element.remove();
+      this.obstacles.forEach(obstacle=>{
+        obstacle.element.remove();
+      });
+
+      this.gameScreen.style.display = "none";
+      this.gameEndScreen.style.display = "block";
+      lives.innerHTML = 0;
+    }
+  }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,94 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game; // added
 
   startButton.addEventListener("click", function () {
     startGame();
   });
 
+  restartButton.addEventListener("click", function() {
+    startGame();
+  });  
+
   function startGame() {
     console.log("start game");
+    game = new Game(); // added
+
+    game.start(); // added
   }
+
+  // Function that handles keys events
+  function handleKeydown (event) {
+    const key = event.key;
+
+    const possibleKeys = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown"
+    ]
+
+    if (possibleKeys.includes(key)) {
+      event.preventDefault();
+
+      if(game){
+        switch(key){
+          case 'ArrowLeft':
+            game.player.directionX = -10;
+            break;
+
+            case 'ArrowUp':
+            game.player.directionY = -10;
+            break;
+
+            case 'ArrowRight':
+            game.player.directionX = 10;
+            break;
+
+            case 'ArrowDown':
+            game.player.directionY = 10;
+            break;
+        }
+      }
+    }
+  }
+
+  function handleKeyup (event) {
+    const key = event.key;
+
+    const possibleKeys = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown"
+    ]
+
+    if (possibleKeys.includes(key)) {
+      event.preventDefault();
+
+      if(game){
+        switch(key){
+          case 'ArrowLeft':
+            game.player.directionX = 0;
+            break;
+
+            case 'ArrowUp':
+            game.player.directionY = 0;
+            break;
+
+            case 'ArrowRight':
+            game.player.directionX = 0;
+            break;
+
+            case 'ArrowDown':
+            game.player.directionY = 0;
+            break;
+        }
+      }
+    }
+  }
+
+  window.addEventListener("keydown", handleKeydown);
+  window.addEventListener("keyup", handleKeyup);
 };

90cc3ed42e3e5bcecab98b7dd8898db50ce1d7b8
David Lucca <david.v.lucca@gmail.com>
2023-10-20 07:32:29 +0100

    Solved lab

--- js/script.js
+++ js/script.js
@@ -1,12 +1,84 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game; // added
 
   startButton.addEventListener("click", function () {
     startGame();
   });
 
-  function startGame() {
-    console.log("start game");
+  restartButton.addEventListener("click", function () {
+    restartGame();
+  })
+  function restartGame() {
+    location.reload();
   }
+
+  function startGame() {
+    // When the Start Game button is clicked, inside the startGame function we should create a new instance of the Game class and start the game by invoking the start() method
+    game = new Game();
+    game.start();
+  }
+
+  // Function that handles keys events
+  function handleKeydown(event) {
+    const key = event.key
+
+    const possibleKeys = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown"
+    ]
+
+      if (possibleKeys.includes(key)){
+        event.preventDefault();
+
+        if (game){
+          switch(key){
+            case 'ArrowLeft':
+            game.player.directionX = -3;break;
+            case 'ArrowUp':
+            game.player.directionY = -3;break;
+            case 'ArrowRight':
+            game.player.directionX = 3;break;
+            case 'ArrowDown':
+            game.player.directionY = 3;break;
+          }
+        }
+      }
+  }
+
+function handleKeyup(event) {
+    const key = event.key
+
+    const possibleKeys = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown"
+    ]
+
+      if (possibleKeys.includes(key)){
+        event.preventDefault();
+
+        if (game){
+          switch(key){
+            case 'ArrowLeft':
+            game.player.directionX = 0;break;
+            case 'ArrowUp':
+            game.player.directionY = 0;break;
+            case 'ArrowRight':
+            game.player.directionX = 0;break;
+            case 'ArrowDown':
+            game.player.directionY = 0;break;
+          }
+        }
+      }
+  }
+  window.addEventListener("keydown", handleKeydown);
+  window.addEventListener("keyup", handleKeyup);
+
+
+
 };

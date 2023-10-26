48cc15567efc9e7f4107bfbf892ff4f3870de7dc
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2023-10-26 18:09:00 +0200

    I think I got it. I'm not 100%, but I think I got it.

--- js/game.js
+++ js/game.js
@@ -1,3 +1,74 @@
 class Game {
-    // code to be added
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = new Player(
+      this.gameScreen,
+      200,
+      500,
+      100,
+      150,
+      "./images/car.png"
+    );
+
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [new Obstacle(this.gameScreen)];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+  }
+
+  start() {
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameLoop();
+  }
+
+  gameLoop() {
+    console.log("in the game loop");
+
+    if (this.gameIsOver) {
+      return;
+    }
+    this.update();
+    window.requestAnimationFrame(() => this.gameLoop()); // Whaaaaaat
+  }
+
+  update() {
+    console.log("In the update");
+    this.player.move();
+
+    const obstacle = this.obstacles[0];
+    obstacle.move();
+
+    if (obstacle.top > this.height) {
+      obstacle.element.remove();
+      this.obstacles = [];
+      this.obstacles.push(new Obstacle(this.gameScreen));
+    }
+    if (this.player.didCollide(obstacle)) {
+      obstacle.element.remove();
+      this.obstacles = [];
+      this.obstacles.push(new Obstacle(this.gameScreen));
+      this.lives--;
+      document.getElementById("lives").innerHTML--;
+    }
+
+    if (!this.lives) {
+      this.endGame();
+    }
+  }
+
+  endGame() {
+    this.gameIsOver = true;
+    this.player.element.remove();
+    this.obstacles[0].element.remove();
+    this.obstacles = [];
+    this.gameScreen.style.display = "none";
+    this.gameEndScreen.style.display = "block";
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  const game = new Game();
 
   startButton.addEventListener("click", function () {
     startGame();
@@ -8,5 +9,69 @@ window.onload = function () {
 
   function startGame() {
     console.log("start game");
+    game.start();
+  }
+
+  restartButton.addEventListener("click", () => {
+    restartGame();
+  });
+
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
+
+  function handleKeyup(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+      switch (key) {
+        case "ArrowLeft":
+        case "ArrowRight":
+          game.player.directionX = 0;
+          break;
+        case "ArrowUp":
+        case "ArrowDown":
+          game.player.directionY = 0;
+          break;
+      }
+    }
+  }
+
+  window.addEventListener("keydown", handleKeydown);
+  window.addEventListener("keyup", handleKeyup);
+
+  function restartGame() {
+    location.reload();
   }
 };

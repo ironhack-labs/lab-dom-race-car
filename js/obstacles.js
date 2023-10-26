2fb32f886733fe12b153fd7d0a40cee7f30eeb03
Isin Kosemen <isinkosemen@gmail.com>
2023-10-26 22:47:21 +0200

    done

---
+++ js/obstacles.js
@@ -0,0 +1,24 @@
+class Obstacle extends Component {
+  constructor(gameScreen) {
+    super(
+      gameScreen,
+      Math.floor(Math.random() * 300 + 70),
+      0,
+      100,
+      150,
+      "./images/redCar.png"
+    );
+  }
+
+  move() {
+    // Move the obstacle down by 3px
+    this.top += 3;
+    // Update the obstacle's position on the screen
+    this.updatePosition();
+  }
+  
+  updatePosition() {
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+  }
+}

6a64ffa5f20a852c6c3161d4ab8d3e53017648dd
Hector Hernandez <hectorahbravo@gmail.com>
2023-11-08 19:54:01 +0100

    done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,96 @@
 class Game {
-    // code to be added
+  // code to be added
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = new Player(
+      this.gameScreen,
+      200,
+      500,
+      100,
+      150,
+      "./images/car.png"
+    );
+    this.obstacle = new Obstacle(this.gameScreen);
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+  }
+
+  start() {
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+    this.gameScreen.position = "relative";
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+
+    this.gameLoop();
+  }
+
+  gameLoop() {
+    console.log("in the game loop");
+
+    // Interrupt the function to stop the loop if "gameIsOver" is set to "true"
+    if (this.gameIsOver) {
+      return;
+    }
+
+    this.update();
+
+    window.requestAnimationFrame(() => this.gameLoop());
+  }
+
+  update() {
+    this.player.move();
+
+    // Check for collision and if an obstacle is still on the screen
+    for (let i = 0; i < this.obstacles.length; i++) {
+      const obstacle = this.obstacles[i];
+      obstacle.move();
+
+      if (this.player.didCollide(obstacle)) {
+        obstacle.element.remove();
+
+        this.obstacles.splice(i, 1);
+        this.lives--;
+        i--;
+      } else if (obstacle.top > this.height) {
+        this.score++;
+        obstacle.element.remove();
+        this.obstacles.splice(i, 1);
+        i--;
+      }
+      document.getElementById("lives").textContent = this.lives;
+      document.getElementById("score").textContent = this.score;
+
+      const marcador = document.createElement("div");
+    }
+
+    if (this.lives === 0) {
+      this.endGame();
+    }
+
+    // Create a new obstacle based on a random probability
+    // when there is no other obstacles on the screen
+    if (Math.random() > 0.98 && this.obstacles.length < 1) {
+      this.obstacles.push(new Obstacle(this.gameScreen));
+    }
+  }
+
+  // Create a new method responsible for ending the game
+  endGame() {
+    this.player.element.remove();
+    this.obstacles.forEach((obstacle) => obstacle.element.remove());
+
+    this.gameIsOver = true;
+
+    // Hide game screen
+    this.gameScreen.style.display = "none";
+    // Show end game screen
+    this.gameEndScreen.style.display = "block";
+  }
 }

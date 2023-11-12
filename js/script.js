9adb71d9632646f799d376de93094dec5bc25cc3
Victor Junquera <victorjunquera@eventalista.es>
2023-11-12 12:54:46 +0100

    done

--- js/script.js
+++ js/script.js
@@ -8,5 +8,59 @@ window.onload = function () {
 
   function startGame() {
     console.log("start game");
+    game = new Game ();
+    
+    game.start()
+  }
+
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+    window.addEventListener("keyup", (event) => {
+      switch (event.code) {
+        case "ArrowRight":
+        case "ArrowLeft":
+        case "ArrowUp":
+        case "ArrowDown":
+          game.player.directionX = 0;
+          game.player.directionY = 0;
+          break;
+        default:
+      }
+    })
+  }
+
+  window.addEventListener("keydown", handleKeydown);
+
+  restartButton.addEventListener("click", function () {
+    restartGame();
+  });
+
+  function restartGame() {
+    location.reload();
   }
 };

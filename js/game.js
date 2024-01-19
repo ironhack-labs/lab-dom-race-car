3ce6fdb93cd0c4dbd7912f3583b19a9836229708
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-19 17:36:49 +0100

    iteration 6 half done

--- js/game.js
+++ js/game.js
@@ -4,13 +4,13 @@ class Game {
     this.gameScreen = document.getElementById("game-screen");
     this.gameEndScreen = document.getElementById("game-end");
     this.player = new Player(
-        this.gameScreen,
-        200,
-        500,
-        100,
-        150,
-        "./images/car.png"
-      );
+      this.gameScreen,
+      200,
+      500,
+      100,
+      150,
+      "./images/car.png"
+    );
     this.height = 600;
     this.width = 500;
     this.obstacles = [];
@@ -19,6 +19,7 @@ class Game {
     this.gameIsOver = false;
     this.gameIntervalId;
     this.gameLoopFrequency = Math.round(1000 / 60);
+    this.lastObstacleTime = 0;
   }
 
   start() {
@@ -42,5 +43,24 @@ class Game {
   update() {
     console.log("in the update");
     this.player.move();
-  }
+
+    const currentTime = Date.now();
+    const timeSinceLastObstacle = currentTime - this.lastObstacleTime;
+
+    if (timeSinceLastObstacle >= 2000) {
+      this.obstacles.push(new Obstacle(this.gameScreen));
+      this.lastObstacleTime = currentTime;
+    }
+    
+    this.obstacles = this.obstacles.filter((obstacle) => {
+        obstacle.move()
+    if(obstacle.top < 700){
+    return true;
+  } else {
+      obstacle.remove();
+      return false;
+    }
+});
+}
+
 }

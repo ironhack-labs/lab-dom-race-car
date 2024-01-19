ebc920734f68e345298d0ca78f28a20feadfa6ce
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-19 11:38:45 +0100

    iteration 3 halfway done, no methods yet

---
+++ js/player.js
@@ -0,0 +1,34 @@
+class Player {
+  constructor(gameScreen, left, top, width, height) {
+this.gameScreen = gameScreen;
+this.left = left;
+this.top = top;
+this.width = width;
+this.height = height;
+this.directionX = 0;
+this.directionY = 0;
+this.element = document.createElement("img");
+this.element.src = "images\car.png";
+this.element.style.position = "absolute";
+this.element.style.width = `${width}px`;
+this.element.style.height = `${height}px`;
+this.element.style.left = `${left}px`;
+this.element.style.top = `${top}px`;
+
+this.gameScreen.appendChild(this.element);
+}
+
+move (){
+
+}
+
+
+updatePosition (){
+
+}
+
+didCollide(obstacle){
+
+}
+
+}

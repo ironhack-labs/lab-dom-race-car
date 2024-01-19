e795ef4fc8c6aaf94bd0b9e77e18da384870d593
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-19 12:27:47 +0100

    iteration 3 done

--- js/player.js
+++ js/player.js
@@ -1,34 +1,62 @@
 class Player {
   constructor(gameScreen, left, top, width, height) {
-this.gameScreen = gameScreen;
-this.left = left;
-this.top = top;
-this.width = width;
-this.height = height;
-this.directionX = 0;
-this.directionY = 0;
-this.element = document.createElement("img");
-this.element.src = "images\car.png";
-this.element.style.position = "absolute";
-this.element.style.width = `${width}px`;
-this.element.style.height = `${height}px`;
-this.element.style.left = `${left}px`;
-this.element.style.top = `${top}px`;
+    this.gameScreen = gameScreen;
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.directionX = 0;
+    this.directionY = 0;
+    this.element = document.createElement("img");
+    this.element.src = "images/car.png";
+    this.element.style.position = "absolute";
+    this.element.style.width = `${width}px`;
+    this.element.style.height = `${height}px`;
+    this.element.style.left = `${left}px`;
+    this.element.style.top = `${top}px`;
+    this.gameScreen.appendChild(element);
+  }
 
-this.gameScreen.appendChild(this.element);
-}
+  move() {
+    this.top += directionY;
+    this.left += this.directionX;
+    if (this.left < 10) {
+        this.left = 10;
+      }
+  
+      if (this.top < 10) {
+        this.top = 10;
+      }
+  
+      if (this.left > this.gameScreen.offsetWidth - this.width - 10) {
+        this.left = this.gameScreen.offsetWidth - this.width - 10;
+      }
+  
+      if (this.top > this.gameScreen.offsetHeight - this.height - 10) {
+        this.top = this.gameScreen.offsetHeight - this.height - 10;
+      }
+      this.updatePosition();
+  }
 
-move (){
+  updatePosition() {
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+  }
 
-}
+  didCollide(obstacle) {
+    const playerRect = this.element.getBoundingClientRect();
+    const obstacleRect = obstacle.element.getBoundingClientRect();
 
-
-updatePosition (){
-
-}
-
-didCollide(obstacle){
-
+    if (
+      playerRect.left < obstacleRect.right &&
+      playerRect.right > obstacleRect.left &&
+      playerRect.top < obstacleRect.bottom &&
+      playerRect.bottom > obstacleRect.top
+    ) {
+      return true;
+    } else {
+      return false;
+    }
 }
 
 }

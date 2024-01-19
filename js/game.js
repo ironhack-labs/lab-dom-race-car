6b4445611bb54a9b5d98f5f1aa896194aad700c1
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-19 11:08:40 +0100

    functions: first iteration, add instances and first function

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
 class Game {
   constructor(startScreen, gameScreen, gameEndScreen) {
-    this.startScreen = startScreen;
-    this.gameScreen = gameScreen;
-    this.gameEndScreen = gameEndScreen;
+    this.startScreen = document.getElementById("start-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
     this.player = null;
     this.height = 600;
     this.width = 500;
@@ -12,5 +12,30 @@ class Game {
     this.gameIsOver = false;
     this.gameIntervalId = null;
     this.gameLoopFrecuency = 1000 / 60;
+  }
+
+  start() {
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+
+    this.startScreen.style.display = "none";
+
+    this.gameScreen.style.display = "block";
+
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrecuency);
+  }
+
+  gameLoop() {
+    this.update();
+
+    if (this.gameIsOver) {
+      clearInterval(this.gameIntervalId);
+    }
+  }
+
+  update() {
+    console.log("update");
   }
 }

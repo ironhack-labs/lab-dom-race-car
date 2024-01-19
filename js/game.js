dfbc196a6fd4226c1087521489b44f960a9842f3
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-19 10:53:05 +0100

    function: add constructor

--- js/game.js
+++ js/game.js
@@ -1,3 +1,16 @@
 class Game {
-    // code to be added
+  constructor(startScreen, gameScreen, gameEndScreen) {
+    this.startScreen = startScreen;
+    this.gameScreen = gameScreen;
+    this.gameEndScreen = gameEndScreen;
+    this.player = null;
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gameIntervalId = null;
+    this.gameLoopFrecuency = 1000 / 60;
+  }
 }

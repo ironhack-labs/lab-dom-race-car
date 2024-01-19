0b7cf7a28a9aa499e2620d9ef3ec00502438927e
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-01-19 12:57:50 +0100

    finish iterations 1 and 2

--- js/game.js
+++ js/game.js
@@ -1,3 +1,37 @@
 class Game {
-    // code to be added
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = null;
+    this.height = 600;
+    this.width = 500;
+    this.obstables = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gameIntervalId;
+    this.gameLoopFrecuency = Math.round(1000 / 60); //60fps
+  }
+
+  start() {
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrecuency);
+  }
+
+  gameLoop() {
+    console.log("in the game loop");
+    this.update();
+    if (this.gameIsOver) {
+      clearInterval(this.gameIntervalId);
+    }
+  }
+  update() {
+    console.log("in the update");
+  }
 }
--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ window.onload = function () {
   const restartButton = document.getElementById("restart-button");
 
   startButton.addEventListener("click", function () {
-    startGame();
+    startGame(); //When clicking on the start button, we want to invoke startGame()
   });
 
   function startGame() {
-    console.log("start game");
+    game = new Game(); //we're instantiating (creating) a new game using the blueprint(constructor/class) called Game
+    game.start(); // Here we're invoking the start() method coming from the blueprint (no need to write "this" because we're not inside the class Game)
   }
 };
+
+

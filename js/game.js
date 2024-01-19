ee2fc854aa5b6d28db0b4b2da1e720964c005938
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-19 11:19:13 +0100

    iteration 1 done and checked

--- js/game.js
+++ js/game.js
@@ -1,34 +1,38 @@
-// constructor() {}
-// new Game()
+
 class Game {
   constructor() {
-    (this.startScreen = document.getElementById("game-intro")),
-      (this.gameScreen = document.getElementById("game-screen")),
-      (this.gameEndScreen = document.getElementById("game-end")),
-      (this.player = null),
-      (this.height = 600),
-      (this.width = 500),
-      (this.obstacles = []),
-      (this.score = 0),
-      (this.lives = 3),
-      (this.gameIsOver = false),
-      (this.gameIntervalId = gameIntervalId),
-      (this.gameLoopFrecuency = 1000 / 60);
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = null;
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gameIntervalId;
+    this.gameLoopFrequency = Math.round(1000 / 60);
   }
 
   start() {
-    this.startScreen.style.classList.remove("show");
-    this.gameScreen.style.classList.add("show");
-    this.setInterval(gameLoop(), gameLoopFrecuency);
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameIntervalId = setInterval(() => {
+        this.gameLoop()
+    }, this.gameLoopFrequency)
   }
 
   gameLoop() {
-    if (gameIsOver) {
-      clearInterval(gameIntervalId);
-      return;
+    console.log("in the game loop");
+    this.update();
+    if (this.gameIsOver) {
+      clearInterval(this.gameIntervalId);
     }
-    this.game.update();
   }
-
-  update() {}
+  update() {
+    console.log("in the update");
+  }
 }

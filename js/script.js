be9b2e30ef7358b2c554d4ce2649e4286cde6961
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-01-19 13:54:07 +0100

    first commit, implement up to iteration 6

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  let game;
   startButton.addEventListener("click", function () {
     startGame();
+    console.log("click");
   });
 
   function startGame() {
-    console.log("start game");
+    game = new Game();
+    game.start();
   }
+
+  document.onkeydown = function (keyPress) {
+    keyPress.preventDefault();
+    if (keyPress.key === "ArrowLeft") {
+      game.player.directionX = 3;
+    } else if (keyPress.key === "ArrowRight") {
+      game.player.directionX = 3;
+    }
+  };
+
+  document.onkeyup = function (keyPress) {
+    keyPress.preventDefault();
+    if (keyPress.key === "ArrowLeft") {
+      game.player.directionX = 0;
+    } else if (keyPress.key === "ArrowRight") {
+      game.player.directionX = 0;
+    }
+  };
 };

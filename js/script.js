7a3b6d72141bf8d00ce44eb4c6018e40267db5a7
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-01-21 22:47:00 +0100

    race car lab

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game = null;
 
   startButton.addEventListener("click", function () {
     startGame();
@@ -8,5 +9,33 @@ window.onload = function () {
 
   function startGame() {
     console.log("start game");
+    game = new Game();
+    game.start();
   }
+
+  window.addEventListener("keydown", (event) => {
+    event.preventDefault();
+    const key = event.key;
+
+    switch (key) {
+      case "ArrowDown":
+        game.player.directionY+=3;
+        return;
+      case "ArrowUp":
+        game.player.directionY-=3;
+        return;
+      case "ArrowLeft":
+        game.player.directionX-=3;
+        return;
+      case "ArrowRight":
+        game.player.directionX+=3;
+        return;
+    }
+
+  });
+
+  document.querySelector("#restart-button").addEventListener("click", () => {
+    location.reload();
+  });
+
 };

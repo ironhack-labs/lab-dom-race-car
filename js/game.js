60536db92c13a29839a862767e841225771e12c9
Ahmet_İnan <ahmet92@gmail.com>
2024-02-08 23:27:32 +0300

    first

--- js/game.js
+++ js/game.js
@@ -1,3 +1,33 @@
 class Game {
-    // code to be added
+constructor(){
+    this.startScreen = document.querySelector("#game-intro");
+    this.gameScreen = document.querySelector("#game-screen");
+    this.gameEndScreen = document.querySelector("#game-end");
+    this.player=null
+    this.height=600
+    this.width=500
+    this.obstacles=[]
+    this.score=0
+    this.lives=3
+    this.gameIsOver=false
+    this.gameIntervalId
+    this.gameLoopFrecuency =Math.round( 1000 / 60);
+    
+}
+
+start()
+{
+this.gameScreen.style.height=`${this.height}px`
+}
+
+gameLoop()
+{
+
+}
+
+update()
+{
+
+}
+
 }

6fee2f5bf80ab024ebd28252ffe1f75787f63b4d
guzegui <guzmanegui@gmail.com>
2024-02-10 14:46:11 +0100

    Done Iteration 4

--- js/game.js
+++ js/game.js
@@ -1,48 +1,55 @@
 class Game {
-    constructor() {
-      this.startScreen = document.getElementById("game-intro");
-      this.gameScreen = document.getElementById("game-screen");
-      this.gameEndScreen = document.getElementById("game-end");
-      this.player = null;
-      this.height = 600;
-      this.width = 500;
-      this.obstacles = [];
-      this.score = 0;
-      this.lives = 3;
-      this.gameIsOver = false;
-      this.gameIntervalId;
-      this.gameLoopFrequency = Math.round(1000/60); // 60fps
-    }
-  
-    start() {
-      // Set the height and width of the game screen
-      this.gameScreen.style.height = `${this.height}px`;
-      this.gameScreen.style.width = `${this.width}px`;
-  
-      // Hide the start screen
-      this.startScreen.style.display = "none";
-      
-      // Show the game screen
-      this.gameScreen.style.display = "block";
-  
-      // Runs the gameLoop on a fequency of 60 times per second. Also stores the ID of the interval.
-      this.gameIntervalId = setInterval(() => {
-        this.gameLoop()
-      }, this.gameLoopFrequency)
-    }
-  
-    gameLoop() {
-      console.log("in the game loop");
-      
-      this.update();
-  
-      // If "gameIsOver" is set to "true" clear the interval to stop the loop
-      if (this.gameIsOver) {
-        clearInterval(this.gameIntervalId)
-      }
-    }
-  
-    update() {
-      console.log("in the update");
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = new Player(
+      this.gameScreen,
+      200,
+      500,
+      100,
+      150,
+      "../images/car.png"
+    );
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gameIntervalId;
+    this.gameLoopFrequency = Math.round(1000 / 60); // 60fps
+  }
+
+  start() {
+    // Set the height and width of the game screen
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+
+    // Hide the start screen
+    this.startScreen.style.display = "none";
+
+    // Show the game screen
+    this.gameScreen.style.display = "block";
+
+    // Runs the gameLoop on a fequency of 60 times per second. Also stores the ID of the interval.
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrequency);
+  }
+
+  gameLoop() {
+    console.log("in the game loop");
+
+    this.update();
+
+    // If "gameIsOver" is set to "true" clear the interval to stop the loop
+    if (this.gameIsOver) {
+      clearInterval(this.gameIntervalId);
     }
   }
+
+  update() {
+    this.player.move();
+  }
+}

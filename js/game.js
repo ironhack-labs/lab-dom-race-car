aa7fbadf5d017dfab1a61ffc49812a8a439707ae
Idir FOUQUET <fouquetidir@gmail.com>
2024-02-12 22:17:07 +0100

    Iteration 1 - 2 - 3 done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,54 @@
 class Game {
-    // code to be added
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = new Player(
+      this.gameScreen,
+      200,
+      500,
+      100,
+      150,
+      "./images/car.png"
+    );
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gameIntervalId;
+    this.gameLoopFrecuency = Math.round(1000 / 60);
+  }
+
+  start() {
+    // Display the Game screen
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+
+    // Hide the strart screen
+    this.startScreen.style.display = "none";
+
+    // Show the strart screen
+    this.gameScreen.style.display = "block";
+
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrequency);
+  }
+
+  gameLoop() {
+    console.log("in the game loop");
+
+    this.update();
+
+    // If "gameIsOver" is set to "true" clear the interval to stop the loop
+    if (this.gameIsOver) {
+      clearInterval(this.gameIntervalId);
+    }
+  }
+
+  update() {
+    console.log("in the update");
+  }
 }

4b73a719b069c65bb3e33720d7a6a2e030c080b9
Arnau Ramos <arnauramos21@gmail.com>
2024-02-13 14:47:41 +0100

    Done iteration 7 and fixed scores + lives issue

--- js/game.js
+++ js/game.js
@@ -14,6 +14,10 @@ class Game {
     this.height = 600;
     this.width = 500;
     this.obstacles = [];
+    
+    this.scoreElement = document.getElementById("score")
+    this.livesElement = document.getElementById("lives")
+
     this.score = 0;
     this.lives = 3;
     this.gameIsOver = false;
@@ -44,7 +48,63 @@ class Game {
   }
 
   update() {
-    this.player.move()
+    this.player.move();
+
+    // Check for collision and if an obstacle is still on the screen
+    for (let i = 0; i < this.obstacles.length; i++) {
+      const obstacle = this.obstacles[i];
+      obstacle.move();
+
+      // If the player's car collides with an obstacle
+      if (this.player.didCollide(obstacle)) {
+        // Remove the obstacle element from the DOM
+        obstacle.element.remove();
+        // Remove obstacle object from the array
+        this.obstacles.splice(i, 1);
+        // Reduce player's lives by 1
+        this.lives--;
+        //Update display
+        this.livesElement.innerText = this.lives;
+        // Update the counter variable to account for the removed obstacle
+        i--;
+      } // If the obstacle is off the screen (at the bottom)
+      else if (obstacle.top > this.height) {
+        // Increase the score by 1
+        this.score++;
+        // Update display
+        this.scoreElement.innerText = this.score;
+        // Remove the obstacle from the DOM
+        obstacle.element.remove();
+        // Remove obstacle object from the array
+        this.obstacles.splice(i, 1);
+        // Update the counter variable to account for the removed obstacle
+        i--;
+      }
+    }
+
+    // If the lives are 0, end the game
+    if (this.lives === 0) {
+      this.endGame();
+    }
+
+    // Create a new obstacle based on a random probability
+    // when there is no other obstacles on the screen
+    if (Math.random() > 0.98 && this.obstacles.length < 1) {
+      this.obstacles.push(new Obstacle(this.gameScreen));
+    }
+  }
+
+  endGame() {
+    this.player.element.remove();
+    this.obstacles.forEach(function (obstacle) {
+      obstacle.element.remove();
+    });
+
+    this.gameIsOver = true;
+    // Hide game screen
+    this.gameScreen.style.display = "none";
+    // Show end game screen
+    this.gameEndScreen.style.display = "block";
   }
 }
 

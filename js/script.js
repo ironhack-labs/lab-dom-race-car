3a83e8991c7b5ffbdbbb91cd68cbb5d1748d7352
Shannon <shannonblas@pucpr.edu>
2024-02-16 14:27:18 -0500

    Solved racecar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  let game;
   startButton.addEventListener("click", function () {
     startGame();
   });
 
+  document.addEventListener("keydown", (e) => {
+    console.log(e);
+    if (e.key === "ArrowDown") {
+      game.player.directionY = 1;
+    }
+    if (e.key === "ArrowUp") {
+      game.player.directionY = -1;
+    }
+    if (e.key === "ArrowLeft") {
+      game.player.directionX = -1;
+    }
+    if (e.key === "ArrowRight") {
+      game.player.directionX = 1;
+    }
+  });
+
   function startGame() {
     console.log("start game");
+    game = new Game();
+    game.start();
   }
 };

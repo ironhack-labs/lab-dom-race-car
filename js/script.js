32d5795d056c36ac08b74efcbbbdbe6ef0a208f1
Jonathan <nathan.diaz062@gmail.com>
2024-02-16 12:21:27 -0400

    Solve Lab

--- js/script.js
+++ js/script.js
@@ -6,7 +6,54 @@ window.onload = function () {
     startGame();
   });
 
+  window.addEventListener("keydown", handleKeydown);
+
+  restartButton.addEventListener("click", function () {
+    // Call the restartGame function when the button is clicked
+    restartGame();
+  });
+
+  
   function startGame() {
     console.log("start game");
+    game = new Game(); // added
+
+    game.start(); // added
   }
+
+    // Function that handles keydown event
+    function handleKeydown(event) {
+      const key = event.key;
+      const possibleKeystrokes = [
+        "ArrowLeft",
+        "ArrowUp",
+        "ArrowRight",
+        "ArrowDown",
+      ];
+  
+      // Check if the pressed key is in the possibleKeystrokes array
+      if (possibleKeystrokes.includes(key)) {
+        event.preventDefault();
+  
+        // Update player's directionX and directionY based on the key pressed
+        switch (key) {
+          case "ArrowLeft":
+            game.player.directionX = -1;
+            break;
+          case "ArrowUp":
+            game.player.directionY = -1;
+            break;
+          case "ArrowRight":
+            game.player.directionX = 1;
+            break;
+          case "ArrowDown":
+            game.player.directionY = 1;
+            break;
+        }
+      }
+    }
+
+    function restartGame() {
+      location.reload();
+    }
 };

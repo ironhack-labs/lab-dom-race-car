164d948bae3ced9bcd31cc535899d6c4907986af
John Pieri <jvpieri@gmail.com>
2024-02-16 20:08:59 -0400

    Basic Game Logic Done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,90 @@
 class Game {
-    // code to be added
+    constructor() {
+        this.startScreen = document.getElementById("game-intro");
+        this.gameScreen = document.getElementById("game-screen")
+        this.gameEndScreen = document.getElementById("game-end")
+        this.player = new Player(this.gameScreen, 200, 390, 100, 150, "./images/car.png")
+        this.height = 600
+        this.width = 500
+        this.obstacles = []
+        this.score = 0
+        this.lives = 3
+        this.gameOver = false
+        this.gameIntervalId;
+        this.gameLoopFrequency = 1000/60
+        this.frames = 0
+    }
+
+    start() {
+        // Set the height and width of the game screen
+        this.gameScreen.style.width = `${this.width}px`
+        this.gameScreen.style.height = `${this.width}px`
+
+        // Hide the start screen
+        this.startScreen.style.display = "none"
+
+        // Show the game screen
+
+        this.gameScreen.style.display = "block"
+
+        // Runs the gameLoop on a fequency of 60 times per second. Also stores the ID of the interval.
+        this.gameIntervalId = setInterval(() => {
+            this.gameLoop()
+        }, this.gameLoopFrequency);
+    }
+
+    gameLoop() {
+        this.frames += 1
+        if (this.frames % 125 === 0) {
+            this.obstacles.push(new Obstacle(this.gameScreen))
+        }
+        this.update()
+
+        // If "gameIsOver" is set to "true" clear the interval to stop the loop
+        if (this.gameIsOver) {
+            clearInterval(this.gameIntervalId)
+        }
+
+    }
+
+    update() {
+        this.player.move()
+        console.log("in the update")
+        this.obstacles.forEach((obstacle, index) => {
+            obstacle.move()
+
+            if (this.player.didCollide(obstacle)) {
+                obstacle.element.remove()
+                this.obstacles.splice(index, 1)
+                this.lives--
+                document.getElementById("lives").innerText = `${this.lives}`
+            } 
+            else if (obstacle.top > this.height) {
+                this.score++
+                obstacle.element.remove()
+                console.log(this.score)
+                document.getElementById("score").innerText = `${this.score}`
+                this.obstacles.splice(index, 1)
+            }
+        })
+        
+    
+        if (this.lives === 0) {
+            this.endGame()
+        }
+        
+    }
+
+    endGame() {
+        this.player.element.remove()
+        this.obstacles.forEach(obstacle => obstacle.element.remove())
+
+        this.gameIsOver = true
+
+        this.gameScreen.style.display = "none"
+
+        this.gameEndScreen.style.display = "block"
+    }
+
+
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,81 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game;
 
   startButton.addEventListener("click", function () {
     startGame();
   });
-
+  restartButton.addEventListener("click", function() {
+    restartGame()
+  })
   function startGame() {
     console.log("start game");
+    game = new Game()
+
+    game.start()
   }
+
+  function restartGame() {
+    location.reload()
+  }
+
+
+  document.addEventListener("keydown", (e) => {
+    const key = e.key
+    const possibleKeyStrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown"
+    ];
+  
+    if (possibleKeyStrokes.includes(key)) {
+      e.preventDefault()
+  
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -3
+          break
+        case "ArrowUp":
+          game.player.directionY = -3
+          break
+        case "ArrowRight":
+          game.player.directionX = 3
+          break
+        case "ArrowDown":
+          game.player.directionY = 3
+          break
+      }
+  
+    }
+  })
+
+  document.addEventListener("keyup", (e) => {
+    let key = e.key
+    e.preventDefault()
+
+    const possibleKeyStrokes = [
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight"
+    ]
+
+    if (possibleKeyStrokes.includes(key)) {
+      switch (key) {
+        case "ArrowUp":
+        case "ArrowDown":
+          game.player.directionY = 0
+          break
+        case "ArrowRight":
+        case "ArrowLeft":
+          game.player.directionX = 0
+          break
+      }
+    }
+
+  })
+  
 };
+

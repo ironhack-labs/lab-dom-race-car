61af65bd99dd3655376ceed153815f09e8e608e9
Dalton <daltongetbaker@gmail.com>
2024-03-21 18:52:30 -0400

    Final race car commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  let game;
   startButton.addEventListener("click", function () {
     startGame();
+  });
+  restartButton.addEventListener("click", function () {
+    startGame();
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowDown") {
+      if (game.player.directionY < 4) {
+        game.player.directionY += 1;
+      }
+    }
+    if (e.key === "ArrowUp") {
+      if (game.player.directionY > -4) {
+        game.player.directionY += -1;
+      }
+    }
+    if (e.key === "ArrowLeft") {
+      if (game.player.directionX > -4) {
+        game.player.directionX += -1;
+      }
+    }
+    if (e.key === "ArrowRight") {
+      if (game.player.directionX < 4) {
+        game.player.directionX += 1;
+      }
+    }
   });
 
   function startGame() {
     console.log("start game");
+    game = new Game();
+    game.start();
   }
 };
